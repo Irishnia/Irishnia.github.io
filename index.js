cb84ddaa8f31098dcf6f3d5cf7ba3dca84daac47
Irishnia <kiirinsage@gmail.com>
2026-05-06 18:10:58 +0100

    insanity

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-async function loadJson() {
+/*async function loadJson() {
   try {
     const response = await fetch('location_data.json');
     
@@ -13,5 +13,33 @@ async function loadJson() {
   } catch (err) {
     console.error('Error loading JSON:', err);
     throw err; // Re-throw so caller can handle it
+  }
+}*/
+
+//Populate Selection with Options
+systemsQuantity = 16;
+for(a=1; a<=systemsQuantity; a++){
+  for(b=0; b<locationDATA[a].length; b++){
+    for(c=0; c<locationDATA[a][b].Resources.length; c++){
+      willAdd = true //why is this not being repeated
+      const target = document.getElementById("searchTarget")
+      for (const child of target.children){
+        if(child.value = locationDATA[a][b].Resources[c]){
+          willAdd = false
+          break
+        }
+      }
+      if(willAdd){
+        opt = document.createElement('option');
+        opt.value = locationDATA[a][b].Resources[c];
+        opt.innerHTML = locationDATA[a][b].Resources[c];
+        target.appendChild(opt);
+      console.log("added resource " + locationDATA[a][b].Resources[c]);
+    }else{
+      willAdd = true; //why is this being skipped?
+      console.log(locationDATA[a][b].Resources[c] + " is already an option")
+    }
+   //console.log(locationDATA[a][b].Resources[c])
+  }
   }
 }
