a18d6f4498a422ee6f70c80bad7734d07722dc63
Irishnia <kiirinsage@gmail.com>
2026-05-15 17:01:50 +0100

    game wiped, marking outdated info

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ for(a=1; a<=systemsQuantity; a++){
   currentSector.appendChild(system);
   for(b=0; b<locationDATA[a].length; b++){
     for(c=0; c<locationDATA[a][b].Resources.length; c++){
-      willAdd = true //why is this not being repeated
+      willAdd = true
       const target = document.getElementById("resourceSearchTarget")
       for (const child of target.children){
         if(child.value == locationDATA[a][b].Resources[c]){
@@ -28,7 +28,7 @@ for(a=1; a<=systemsQuantity; a++){
         target.appendChild(opt);
       //console.log("added resource " + locationDATA[a][b].Resources[c]);
     }else{
-      willAdd = true; //why is this being skipped?
+      willAdd = true;
       //console.log(locationDATA[a][b].Resources[c] + " is already an option")
     }
    //console.log(locationDATA[a][b].Resources[c])
